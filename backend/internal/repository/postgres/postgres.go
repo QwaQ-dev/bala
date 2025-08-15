bc8c6442073e744f5d1839a515e78db546a9cb55
qwaqd <qwaq.dev@gmail.com>
2025-08-15 23:32:16 +0500

    remove migrations down

--- backend/internal/repository/postgres/postgres.go
+++ backend/internal/repository/postgres/postgres.go
@@ -55,8 +55,6 @@ func InitDatabase(cfg config.Database, log *slog.Logger) (*sql.DB, error) {
 		return nil, err
 	}
 
-	runMigrations(m, "down", log)
-
 	runMigrations(m, "up", log)
 
 	log.Info("Database connected successfully")
